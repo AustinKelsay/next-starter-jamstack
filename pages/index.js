c2f01f806a8b6ecfc578d4891d02315270e53a36
austinkelsay <austinkelsay@yahoo.com>
2021-03-10 08:45:42 -0600

    Inital commit with first changes

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,19 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet" /> 
+    </Head>
+    <h1 className='header'>Reclaim</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    <h3>Reclaim is an open source desktop app for your personal online information. With Reclaim you can collect your social media data in a few clicks and explore every photo, friend, and intereaction from your digital life.</h3>
+
+    <button className='btn'>Reclaim your data</button>
+    <div className='input-container'>
+      <h4>Join our email list to get early access to features, updates, and exclusive discounts as we expand Reclaim</h4>
+      <input type='text' placeholder='example@email.com' />
+    </div>
 
     <style jsx>{`
       :global(html,body) {
@@ -16,8 +26,7 @@ const Home = () => (
 
       :global(body) {
         font-size: calc(10px + 1vmin);
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
-          'Droid Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'Source Code Pro', monospace;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
 
@@ -29,14 +38,26 @@ const Home = () => (
         background-color: #282c34;
         color: white;
       }
-
-      a {
-        color: pink;
-        text-decoration: none;
+      .header {
+        font-weight: 800;
       }
-
-      .content {
-        padding: 0 32px;
+      h3 {
+        margin-bottom: 5%;
+      }
+      .btn {
+        padding: 1%;
+        margin-bottom: 5%;
+        background: linear-gradient(to bottom right, #d7d6d6, #6e54c4);
+        border: 1px solid white;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      .btn:hover {
+        opacity: 0.8;
+      }
+      .input-container {
+        margin: 3% auto;
+        margin-top: 15%;
       }
     `}</style>
   </div>
