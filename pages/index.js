2efc72f7d54a34cc5deb44d6a8d2fb3957741c62
austinkelsay <austinkelsay@yahoo.com>
2021-03-11 18:28:51 -0600

    Final spacing and style update

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,10 @@ const Home = () => {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet" /> 
         <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Roboto:ital,wght@0,300;1,300&family=Source+Code+Pro:ital,wght@1,300&display=swap" rel="stylesheet" /> 
       </Head>
-      <h1 className='header'>Reclaim</h1>
       <div className='top-container'>
+        <h1 className='header'>Reclaim</h1>
         <h3>Reclaim is an open source desktop app for your personal online information. With Reclaim you can collect your social media data in a few clicks and explore every photo, friend, and interaction from your digital life.</h3>
       </div>
       <iframe className='email-component' src="https://c7ad2f4f.sibforms.com/serve/MUIEAEtphHQDTuZROGG-FTmoBbEmkeYkn4e-Hn292_wis9MqSZtOvpsg1pYE7KCUQYab172rp8s6-vWRWod91iDPoKAc4jTfUwX899YIwD1ThXKCHNkulD0n4BgnMw0b3UBS72Gs1bZLYkmLxgqyqZEUOYXG_RioWHq84hFEmbD1y9gHTv7ePSGC_kAW9LzGhPopL2AoPWn28X2H" 
@@ -25,6 +26,8 @@ const Home = () => {
     
     :global(body) {
       font-size: calc(10px + 1vmin);
+      font-family: 'Inconsolata', monospace;
+      font-family: 'Roboto', sans-serif;
       font-family: 'Source Code Pro', monospace;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
@@ -32,7 +35,7 @@ const Home = () => {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: space-between;
+      justify-content: space-around;
       text-align: center;
       background-color: #282c34;
       color: white;
@@ -40,6 +43,8 @@ const Home = () => {
     .header {
       font-family: Audiowide;
       font-weight: 800;
+      text-decoration: underline;
+      font-size: 3rem;
     }
     h3 {
       margin-bottom: 1%;
@@ -60,7 +65,7 @@ const Home = () => {
       opacity: 0.8;
     }
     .top-container {
-      width: 85%;
+      width: 82%;
       padding: 0.1%;
       margin: 1% auto;
       margin-bottom: 3%;
