70b0662a7854fff00e9e6a16cd45d95864f3c7c9
austinkelsay <austinkelsay@yahoo.com>
2021-03-11 19:13:06 -0600

    Basic about page setup

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 const Home = () => {
   return (
@@ -12,10 +13,21 @@ const Home = () => {
       </Head>
       <div className='top-container'>
         <h1 className='header'>Reclaim</h1>
+        <Link href="/about">
+          <button className='btn'>About us</button>
+        </Link>
         <h3>Reclaim is an open source desktop app for your personal online information. With Reclaim you can collect your social media data in a few clicks and explore every photo, friend, and interaction from your digital life.</h3>
       </div>
-      <iframe className='email-component' src="https://c7ad2f4f.sibforms.com/serve/MUIEAEtphHQDTuZROGG-FTmoBbEmkeYkn4e-Hn292_wis9MqSZtOvpsg1pYE7KCUQYab172rp8s6-vWRWod91iDPoKAc4jTfUwX899YIwD1ThXKCHNkulD0n4BgnMw0b3UBS72Gs1bZLYkmLxgqyqZEUOYXG_RioWHq84hFEmbD1y9gHTv7ePSGC_kAW9LzGhPopL2AoPWn28X2H" 
-      frameborder="0" scrolling="auto" allowfullscreen style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%'}}></iframe>
+      <div className='about-container'>
+        <div className='about-card'>
+            <h2>Austin Kelsay</h2>
+            <p className='about-text'>Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum</p>
+        </div>
+        <div className='about-card'>
+            <h2>Royer Adames</h2>
+            <p className='about-text'>Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum</p>
+        </div>
+      </div>
     
     <style jsx>{`
     :global(html,body) {
@@ -45,34 +57,41 @@ const Home = () => {
       font-weight: 800;
       text-decoration: underline;
       font-size: 3rem;
+      margin-top: 0.5%;
+      margin-bottom: 0;
     }
     h3 {
       margin-bottom: 1%;
     }
     .btn {
-      padding: 1%;
-      margin-bottom: 1%;
-      background: linear-gradient(to bottom right, #d7d6d6, #6e54c4);
-      border: 1px solid white;
-      border-radius: 5px;
-      cursor: pointer;
-      font-family: 'Source Code Pro', monospace;
-      color: black;
-      font-size: 1rem;
-      font-weight: 800;
-    }
-    .btn:hover {
-      opacity: 0.8;
-    }
+        padding: 0.4%;
+        background: linear-gradient(to bottom right, #d7d6d6, #6e54c4);
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      .btn:hover {
+        opacity: 0.8;
+      }
     .top-container {
       width: 82%;
       padding: 0.1%;
       margin: 1% auto;
-      margin-bottom: 3%;
+      margin-bottom: 10%;
     }
-    .email-component {
-      height: 69vh;
-      width: 35vw;
+    .about-container {
+        width: 90%;
+        margin: 1% auto;
+        margin-bottom: 13%;
+        display: flex;
+        flex-direction: row;
+        justify-contetn: space-between;
+    }
+    .about-card {
+        width: 30%;
+        padding: 1%;
+        margin: 1% auto;
+        border: 1px solid white;
+        border-radius: 5px;
     }
     `}</style>
     </div>
