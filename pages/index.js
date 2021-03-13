1fbd7f8a3a05ed440fcf5b82763e85b55ae1073a
austinkelsay <austinkelsay@yahoo.com>
2021-03-13 10:24:10 -0600

    Trying out more simple style with info in the email component

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ const Home = () => {
       </Head>
       <div className='top-container'>
         <h1 className='header'>Reclaim</h1>
-        <h3>Reclaim is the hub for your personal data. Reclaim is an open source desktop app for collecting and storing your online information. With Reclaim you can collect your social media data in a few clicks and explore every photo, friend, and interaction from your digital life.</h3>
+        <h3 className='tagline'>A hub for your personal data</h3>
       </div>
-      <iframe className='email-component' src="https://c7ad2f4f.sibforms.com/serve/MUIEAEtphHQDTuZROGG-FTmoBbEmkeYkn4e-Hn292_wis9MqSZtOvpsg1pYE7KCUQYab172rp8s6-vWRWod91iDPoKAc4jTfUwX899YIwD1ThXKCHNkulD0n4BgnMw0b3UBS72Gs1bZLYkmLxgqyqZEUOYXG_RioWHq84hFEmbD1y9gHTv7ePSGC_kAW9LzGhPopL2AoPWn28X2H" 
+      <iframe className='email-component' src="https://c7ad2f4f.sibforms.com/serve/MUIEAMFL1lYUdyC3XnM4eyOZU-8G2r81zvAGgqRChD1mR8AO-tnipDMZglKxWPzQBMQh5cG2eRJYsILnDsMyUZK9ej1Jv8JhDwbs3i1yB4zVYtfQu0YvaDI5Rc87mZVwCMrIy8xgNgQ8DQHV1v89OMKP601oxTmB7IFA6Gu0sE_ELMDDNdU5MTTO5QLM8YdAx9MoZB71QeiRgrsq" 
       frameBorder="0" scrolling="auto" allowFullScreen style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%'}}></iframe>
     
     <style jsx>{`
@@ -35,7 +35,7 @@ const Home = () => {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: space-around;
+      justify-content: space-between;
       text-align: center;
       background-color: #282c34;
       color: white;
@@ -43,13 +43,19 @@ const Home = () => {
     .header {
       font-family: Audiowide;
       font-weight: 800;
-      text-decoration: underline;
       font-size: 3rem;
       margin-top: 0.5%;
       margin-bottom: 0;
     }
     h3 {
       margin-bottom: 1%;
+      margin-top: 1%;
+      font-size: 1.1rem;
+    }
+    .tagline {
+      margin-bottom: 1%;
+      margin-top: 1%;
+      font-size: 1.4rem;
     }
     .btn {
       padding: 0.4%;
@@ -62,120 +68,12 @@ const Home = () => {
     }
     .top-container {
       width: 82%;
-      padding: 0.1%;
       margin: 1% auto;
-      margin-bottom: 2%;
+      margin-bottom: 3%;
     }
     .email-component {
-      height: 69vh;
-      width: 40vw;
-    }
-    @media(max-width: 1600px) {
-      .top-container {
-        margin-bottom: 1%;
-      }
-    }
-    @media(max-width: 1200px) {
-      .email-component {
-        height: 69vh;
-        width: 50vw;
-      }
-    }
-    @media(max-width: 1000px) {
-      .email-component {
-        width: 55vw;
-      }
-    }
-    @media(max-width: 850px) {
-      .email-component {
-        width: 60vw;
-      }
-    }
-    @media(max-width: 750px) {
-      .email-component {
-        width: 65vw;
-      }
-    }
-    @media(max-width: 650px) {
-      .email-component {
-        width: 70vw;
-      }
-    }
-    @media(max-width: 550px) {
-      .email-component {
-        width: 75vw;
-      }
-    }
-    @media(max-width: 450px) {
-      .email-component {
-        width: 85vw
-      }
-      .header {
-        margin-top: 15%;
-      }
-      .top-container {
-        width: 95%;
-      }
-      h3 {
-        margin-top: 0%;
-      }
-    }
-    @media(max-width: 350px) {
-      .email-component {
-        width: 85vw
-      }
-    }
-    @media(max-width: 330px) {
-      .email-component {
-        width: 90vw;
-      }
-    }
-    @media(max-height: 850px) {
-      .top-container {
-        margin-bottom: 0;
-      }
-    }
-    @media(max-height: 750px) {
-      .header {
-        margin-top: 2%;
-      }
-      .email-component {
-        height: 55vh;
-      }
-    }
-    @media(max-height: 650px) {
-      .header {
-        margin-top: 2%;
-      }
-      .email-component {
-        height: 50vh;
-      }
-    }
-    @media(max-height: 550px) {
-      .email-component {
-        height: 40vh;
-      }
-    }
-    @media(max-height: 450px) {
-      .header {
-        margin-top: 3%;
-      }
-      .email-component {
-        height: 35vh;
-      }
-    }
-    @media(max-height: 350px) {
-      .header {
-        margin-top: 5%;
-      }
-      .email-component {
-        height: 30vh;
-      }
-    }
-    @media(max-height: 300px) {
-      .email-component {
-        height: 25vh;
-      }
+      height: 80vh;
+      width: 90vw;
     }
     `}</style>
     </div>
